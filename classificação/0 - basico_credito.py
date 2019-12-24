027749348829dfe28a115e34ff10376c4c81c5dd
MuriloVS <40902924+MuriloVS@users.noreply.github.com>
2019-12-23 22:57:13 -0300

    pequena atualização
    Autalização do arquivo inicial utilizado para a base de crédito

--- classificação/0 - basico_credito.py
+++ classificação/0 - basico_credito.py
@@ -6,6 +6,7 @@ Este é um arquivo de script temporário.
 """
 
 import pandas as pd
+import numpy as np
 
 # criando a base de dados
 base = pd.read_csv('credit_data.csv')
@@ -18,9 +19,16 @@ base.loc[pd.isnull(base.age) == True, 'age'] = ageMedia
 # criando os previsores e a classe (resultado a ser analisado)
 # iloc[linhas, colunas]
 previsores = base.iloc[:, 1:4].values
-classe = base.iloc[:, 4]
+classe = base.iloc[:, 4].values
 
+# outra maneira de lidar com valores faltantes em 'age'
 from sklearn.impute import SimpleImputer
-
-imputer = SimpleImputer()
+# os parâmetros passados em SimpleImputer podem ser ignorados
+# (são os valores padrão)
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 previsores = imputer.fit_transform(previsores)
+
+# escalonamento dos atributos
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
