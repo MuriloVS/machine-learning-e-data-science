3ae9e9b3399768d86a24ee5a22b174822a187086
MuriloVS <40902924+MuriloVS@users.noreply.github.com>
2020-09-09 22:01:11 -0300

    pequenas correções e ajustes

--- classificação/7 - regressao_logisitica_credito.py
+++ classificação/7 - regressao_logisitica_credito.py
@@ -7,8 +7,8 @@ Created on Tue Jan 14 18:47:43 2020
 Algoritmo de Regressão Logística: apesar do nome, este é um algoritmo de
    classificação (trabalha com rótulos) e não um algoritmo de regressão
    (trabalha com números). Exemplo: uma pessoa vaiu pagar um empréstimo?
-   Algoritmo de classificação (a resposta, ou o que quer ser previsto, se
-   resume aos rótulo 'sim' e 'não'). Qual o limite de crédito deste cliente?
+   Algoritmo de classificação (a resposta, ou o que queremos que seja
+   previsto, se resume aos rótulo 'sim' e 'não'). Qual o limite de crédito deste cliente?
    500? 1000? Estes rótulos são numéricos, seria necessário usar um método/
    algoritmo de predição de regressão.
    Encontra e melhor função sigmóide para os dados.
--- classificação/g - regressao_logisitica_census.py
+++ classificação/g - regressao_logisitica_census.py
@@ -8,8 +8,9 @@ Algoritmo de Regressão Logística.
 
 Precisão:
   - Todos os tratamentos, max_iter=100: 0,8505
-  - Todos os tratamentos, max_iter=200: 0,8507
-  - Sem StandardScaler, max_iter=200: 0,7958
+  - Todos os tratamentos, max_iter=200: 0,8508
+  - Todos os tratamentos, max_iter=400: 0,8508
+  - Sem StandardScaler, max_iter=200: 0,7949
 
 """
 
@@ -19,18 +20,23 @@ import pandas as pd
 base = pd.read_csv('census.csv')
 
 # previsores e classe
-previsores = base.iloc[:, :14].values
+previsores = base.iloc[:, 0:14].values
 classe = base.iloc[:, 14].values
+                
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 # transformação de variáveis categóricas (não numéricas)
-# colunas 1, 3, 5, 6, 7, 8, 9, 13 e 14(classe)
-from sklearn.preprocessing import LabelEncoder
-labelEncoder = LabelEncoder()
-classe = labelEncoder.fit_transform(classe)
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-oneHotEncorder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [1,3,5,6,7,8,9,13])], remainder='passthrough')
-previsores = oneHotEncorder.fit_transform(previsores).toarray()
+# colunas 1, 3, 5, 6, 7, 8, 9, 13 e 14 (classe)
+columns = []
+for i in range(len(previsores[0])):
+    if type(previsores[:, i][0]) == type('str'):
+        columns.append(i)
+column_tranformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), columns)],remainder='passthrough')
+previsores = column_tranformer.fit_transform(previsores).toarray()
+
+labelencorder_classe = LabelEncoder()
+classe = labelencorder_classe.fit_transform(classe)
 
 # padronizando os valores dos previsores
 from sklearn.preprocessing import StandardScaler
@@ -39,15 +45,20 @@ previsores[:, 102:] = scaler.fit_transform(previsores[:, 102:])
 
 # separação entre base de treinamento e testes
 from sklearn.model_selection import train_test_split
-previsoresTreinamento, previsoresTeste, classeTreinamento, classeTeste = train_test_split(previsores, classe, random_state=0)
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, random_state=0)
 
 # treinamento
 from sklearn.linear_model import LogisticRegression
-classificador = LogisticRegression(max_iter=200)
-classificador.fit(previsoresTreinamento, classeTreinamento)
-resultado = classificador.predict(previsoresTeste)
+classificador = LogisticRegression(solver='lbfgs', max_iter=400)
+classificador.fit(previsores_treinamento, classe_treinamento)
+resultado = classificador.predict(previsores_teste)
+# print(classificador.intercept_)
+# print(classificador.coef_)
 
 # análise dos resultados
 from sklearn.metrics import confusion_matrix, accuracy_score
-matriz = confusion_matrix(classeTeste, resultado)
-precisao = accuracy_score(classeTeste, resultado)
+precisao = accuracy_score(classe_teste, resultado)
+matriz = confusion_matrix(classe_teste, resultado)
+
+import collections
+collections.Counter(classe_teste)
